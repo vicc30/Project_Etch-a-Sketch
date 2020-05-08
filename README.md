01ac8ed87b35104e34be3b968ffd195503a8bde6
Victor Cruz <vicc23@gmail.com>
2020-05-08 18:03:28 -0500

    Added all files HTML CSS and JS for project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project_Etch-a-Sketch
 Browser version of something between a sketchpad and an Etch-A-Sketch
+
+This project is done for The Odin Project Web Development program.
+
+Thanks to https://www.theodinproject.com/
+
+Here is a browser version of something between a sketchpad and an Etch-A-Sketch.
+
---
+++ app.js
@@ -0,0 +1,53 @@
+
+let container = document.querySelector('#container');
+let getCell = document.getElementsByClassName("inside");
+let column = 16;
+let row = 16;
+let options = "";
+
+createGrid(row,column);
+selectCell();
+buttonReset();
+
+
+function buttonReset(){ //function that shows if reset button is clicked
+    let resetButton=document.getElementById('reset');
+    resetButton.addEventListener("click", function(){
+        let options = window.prompt("how many squares per side to make the new sketch?",16);
+       container.innerHTML='';
+       createGrid(options,options);
+       selectCell();
+        
+    });
+}
+
+
+
+function selectCell(){ //function that selects a cell with getCell variable
+
+for(var x=0; x<getCell.length; x++){
+    
+    getCell[x].onmouseover= function(){
+        this.classList.add('playing'); //Add CSS rule playing
+    }
+}
+}
+
+
+function createGrid (row,column){ //creates with CSS a Grid sized 'row' x 'column'
+    let newRow=row;
+    let newColumn=column;
+    let cell = newColumn*newRow;
+    console.log(row);
+    console.log(column);
+    container.style.setProperty("--row-size",newRow);
+    container.style.setProperty("--column-size",newColumn);
+        for (let total = 0; total<cell; total++ ){
+        let creation = document.createElement("div");//creates a div element in each cell
+        creation.setAttribute("class","inside"); //gives the class inside to all new div elements for CSS rules.
+        container.appendChild(creation); 
+        } 
+}
+
+
+
